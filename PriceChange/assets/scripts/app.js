072a03ce7cec9a12482bd8fb9405c58ee936df53
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-07-18 20:09:41 +0530

    added price percentage change

--- PriceChange/assets/scripts/app.js
+++ PriceChange/assets/scripts/app.js
@@ -4,6 +4,7 @@ let DP = document.getElementById("dp");
 let FIP = document.getElementById("fip");
 let FDP = document.getElementById("fdp");
 let CALCULATE = document.getElementById("calculate");
+let RESET = document.getElementById("reset");
 
 let calculatePrice=()=>{
     let cp = (+CP.value);
@@ -15,6 +16,14 @@ let calculatePrice=()=>{
     FIP.value=fip;
     FDP.value=fdp;
 }
+let restHandler =()=>{
+    CP.value ='';
+    IP.value='';
+    DP.value='';
+    FIP.value='';
+    FDP.value='';
+}
 
 CALCULATE.addEventListener('click',calculatePrice)
+RESET.addEventListener('click',restHandler);
 
