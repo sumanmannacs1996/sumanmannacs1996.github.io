4766cf38753f759eb36d04b5e43601a018ac5129
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-07-20 22:10:45 +0530

    added project progress bar and expending cards

---
+++ ProgressSteps/app.js
@@ -0,0 +1,43 @@
+const PROGRESS_BAR = document.getElementById('progress');
+const PREV = document.getElementById('prev');
+const NEXT = document.getElementById("next");
+const CIRCLES = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+const update =()=>{
+    let percenrage = ((currentActive-1)/(CIRCLES.length-1))*100 + "%";
+    PROGRESS_BAR.style.width= percenrage;
+    
+    CIRCLES.forEach((cercle,idx)=>{
+        if(idx < currentActive){
+            cercle.classList.add('active');
+            //console.log(currentActive,idx);
+        }
+        else{
+            cercle.classList.remove('active');
+        }
+    })
+
+    if(currentActive ===1){
+        PREV.disabled = true;
+    }else if(currentActive === CIRCLES.length){
+        NEXT.disabled = true;
+    }else{
+        PREV.disabled = false;
+        NEXT.disabled = false;
+    }
+}
+
+NEXT.addEventListener('click',()=>{
+    if(CIRCLES.length > currentActive){
+        currentActive++;
+        update();
+    }
+});
+PREV.addEventListener('click',()=>{
+    if(1 < currentActive){
+        currentActive--;
+        update();
+    }
+});
