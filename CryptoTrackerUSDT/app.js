d298100958c1b0d3594b47cf3bcce58ccec5f6e8
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-08-02 20:46:08 +0530

    added project Crypto Tracker USDT

---
+++ CryptoTrackerUSDT/app.js
@@ -0,0 +1,52 @@
+const currentPrice = document.getElementById('cp');
+const conclusion = document.getElementById('conclusion');
+let USDT_PRICE;
+
+const BuySell = document.getElementById("mybuysellswitch");
+
+const OnOff = document.getElementById('myonoffswitch');
+OnOff.checked = false;
+
+const priceAlert =()=>{
+    if(OnOff.checked){
+        if(BuySell.checked){
+            let targetPrice =(+ document.getElementById('target').value); 
+            conclusion.innerHTML =`Waiting for Buy USDT!!`; 
+            if(targetPrice >= USDT_PRICE){
+                conclusion.innerHTML =`It's time to Buy USDT!!`;
+                document.getElementById("tada").play();
+            }
+        }
+        else{
+            let targetPrice =(+ document.getElementById('target').value);
+            conclusion.innerHTML =`Waiting for Sell USDT!!`; 
+            if(targetPrice <= USDT_PRICE){
+                conclusion.innerHTML =`It's time to Sell USDT!!`;
+                document.getElementById("tada").play();
+            }
+         }
+    }
+}
+
+
+
+
+OnOff.addEventListener('click',priceAlert);
+
+
+
+let getPrice=()=>{
+    fetch('https://public.coindcx.com/market_data/trade_history?pair=I-USDT_INR&limit=1')
+    .then(res=>{
+        return res.json();
+    }).then(data=>{
+        USDT_PRICE = (+ data[0].p);
+        currentPrice.innerHTML = `Current price of USDT ${data[0].p}`
+        priceAlert();
+    })
+}
+
+getPrice();
+setInterval(()=>{
+    getPrice();
+},5000);
